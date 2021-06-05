239d6729d6e1bf12d71062ca5b9bcdc52eabd55d
Henry Ebomah <henryebomah@gmail.com>
2021-06-05 10:33:27 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+printInstructions([
+    "Remove the plastic wrapper",
+    "Microwave 45 seconds",
+    "Let cool",
+    "Enjoy!",
+  ]);
+  
+  // Write your function 'printInstructions' here
+  function printInstructions(steps){
+    for (let i = 0; i < steps.length[i]; i++);{
+  
+  
+  console.log("Step 1 - Remove the plastic wrapper");
+  console.log("Step 2 - Microwave 45 seconds");
+  console.log("Step 3 - Let cool");
+  console.log("Step 4 - Enjoy!");
+    }
+  }
+  // "Step 1 - Remove the plastic wrapper"
+  // "Step 2 - Microwave 45 seconds"
+  // "Step 3 - Let cool"
+  // "Step 4 - Enjoy!"
